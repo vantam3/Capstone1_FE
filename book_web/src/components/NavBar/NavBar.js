718c89f091f72e5ff42934ab7c4d6063f2a31330
Tam Tran <vtam0805@gmail.com>
2024-11-12 20:01:15 +0700

    update

--- book_web/src/components/NavBar/NavBar.js
+++ book_web/src/components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ function Navbar() {
             <div className="nav-links">
                 <Link to="/">Home</Link>
                 <Link to="/library">Library</Link>
-                <Link to="/create">Create Books</Link>
+                <Link to="/create">Create</Link>
                 <Link to="/about">About</Link>
             </div>
             <div className="nav-right">
